ca713d1fdc6fe211b38e7bd27c0b1ab9911adff1
akim-ruslanov <akim.ruslanov@gmail.com>
2025-10-20 17:57:06 -0700

    Serve under game-of-life

--- app.js
+++ app.js
@@ -5,20 +5,15 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(__filename)
-console.log(__dirname)
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Serve CRA build folder
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve build folder
+app.use('/game-of-life', express.static(path.join(__dirname, 'build')));
 
-// For all other routes, serve index.html (CRA client-side routing)
-app.get('*', (req, res) => {
+// For all other routes, serve index.html
+app.get('/game-of-life/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Game of Life CRA app running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Game of Life app running on port ${PORT}`));
